1de0136fe2af96dcdeb9ff9041d007b195752ff6
cmathis94 <collin.mathis94@gmail.com>
2023-07-21 15:26:23 -0500

    forcing SSL and trying error catching

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const secure = require('ssl-express-www');
+
+// const AppError = require('./util/appError');
+// const catchAsync = require('./util/catchAsync');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,9 +15,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/assets'));
 app.use(express.urlencoded({ extended: true }));
+app.use(secure);
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.use((err, req, res, next) => {
+  const { status, message = 'Something Went Wrong' } = err;
+  res.status(status).send(message);
 });
 
 app.listen(port, () => {
