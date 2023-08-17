7d464a90f2a0120361a24fa1c6368ca7c8a42e91
cmathis94 <collin.mathis94@gmail.com>
2023-08-17 10:56:33 -0500

    updated projects and added service page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const secure = require('ssl-express-www');
+// const secure = require('ssl-express-www');
 
 // const AppError = require('./util/appError');
 // const catchAsync = require('./util/catchAsync');
@@ -15,16 +15,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/assets'));
 app.use(express.urlencoded({ extended: true }));
-app.use(secure);
+// app.use(secure);
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.use((err, req, res, next) => {
-  const { status, message = 'Something Went Wrong' } = err;
-  res.status(status).send(message);
+app.get('/services', (req, res) => {
+  res.render('services');
 });
+
+// app.use((err, req, res, next) => {
+//   const { status, message = 'Something Went Wrong' } = err;
+//   res.status(status).send(message);
+// });
 
 app.listen(port, () => {
   console.log(`On port ${port}`);
